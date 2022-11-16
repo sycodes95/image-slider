e0350ed0e2a199e2cdd778c6c006e38ef9cd539e
sycodes95 <sycodes95@gmail.com>
2022-11-16 15:04:22 -0800

    Image slider fully functional

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const buttons = document.querySelectorAll('[data-slide-button]');
+const navButtons = document.querySelectorAll('.nav');
+const slide = document.querySelectorAll('.slide')
+const slideOne = document.querySelector('.slide.one')
+const slideTwo = document.querySelector('.slide.two')
+const slideThree = document.querySelector('.slide.three')
+const navOne = document.querySelector('.nav.one')
+const navTwo = document.querySelector('.nav.two')
+const navThree = document.querySelector('.nav.three')
+
+
 
 buttons.forEach(e => {
-    e.addEventListener('click', ()=> {
+
+    
+    e.addEventListener('click', ()=> { 
+        navButtons.forEach(e =>{
+            e.style.backgroundColor = 'white'
+        })
+
         const offset = e.dataset.slideButton === 'next' ? 1 : -1;
         const slides = e.closest('[data-container').querySelector('[data-slide-container]')
     
@@ -13,5 +29,83 @@ buttons.forEach(e => {
 
         slides.children[newIndex].dataset.active = true;
         delete activeSlide.dataset.active
+        console.log(newIndex)
+
+        console.log(navOne)
+
+        console.log(slideOne.dataset)
+
+        if (slideOne.dataset.active == 'true'){
+            navOne.style.backgroundColor = 'rgba(52, 149, 205, 0.5)'
+            
+                
+        } else if (slideTwo.dataset.active == 'true'){
+            navTwo.style.backgroundColor = 'rgba(52, 149, 205, 0.5)'
+
+        } else if (slideThree.dataset.active == 'true'){
+            navThree.style.backgroundColor = 'rgba(52, 149, 205, 0.5)'
+        }
+
+        
+
+        
     }) 
 })
+
+navOne.addEventListener('click', ()=>{
+    delete slideTwo.dataset.active;
+    delete slideThree.dataset.active;
+
+    navButtons.forEach(e =>{
+        e.style.backgroundColor = 'white'
+    });
+    navOne.style.backgroundColor = 'rgba(52, 149, 205, 0.5)'
+    slideOne.dataset.active = true;
+    //delete slideOne.dataset.active
+    console.log(slideOne.dataset.active)
+    console.log(slideTwo.dataset.active)
+    console.log(slideThree.dataset.active)
+})
+
+navTwo.addEventListener('click', ()=>{
+    delete slideOne.dataset.active;
+    delete slideThree.dataset.active;
+
+    navButtons.forEach(e =>{
+        e.style.backgroundColor = 'white'
+    });
+    navTwo.style.backgroundColor = 'rgba(52, 149, 205, 0.5)'
+    
+    slideTwo.dataset.active = true;
+
+    console.log(slideOne.dataset.active)
+    console.log(slideTwo.dataset.active)
+    console.log(slideThree.dataset.active)
+    
+
+    //delete slideTwo.dataset.active
+
+})
+
+navThree.addEventListener('click', ()=>{
+
+    delete slideOne.dataset.active;
+    delete slideTwo.dataset.active;
+    navButtons.forEach(e =>{
+        e.style.backgroundColor = 'white'
+    });
+    navThree.style.backgroundColor = 'rgba(52, 149, 205, 0.5)'
+    
+    slideThree.dataset.active = true;
+
+    console.log(slideOne.dataset.active)
+    console.log(slideTwo.dataset.active)
+    console.log(slideThree.dataset.active)
+
+    //delete slideThree.dataset.active
+
+})
+
+
+
+
